608e3da68073683d24c05d97621a898c28ef70eb
CatalinaChirtes <adela.chirtes@gmail.com>
2022-12-27 21:55:14 +0200

    fixed bug in main, and started working on cp command

--- cp.cpp
+++ cp.cpp
@@ -1,7 +1,11 @@
-#include <unistd.h>
+#include <string.h>
 #include <string>
 #include <getopt.h>
 #include <iostream>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <fstream>
 
 using namespace std;
 
@@ -9,8 +13,112 @@ struct option long_options[] = {
         {"help", no_argument, 0, 'h'}
 };
 
+int isDir(const char* fileName)
+{
+    struct stat path;
+    stat(fileName, &path);
+    return S_ISREG(path.st_mode);
+}
+
+void argType(int argc, char **argv,char **sourceArray,char *destination)
+{
+    for(int i=1;i<argc;i++)
+        if(argv[i][0] == '-')
+        {
+            if(argv[i][1] == 't')
+            {
+                int count = 0;
+                for (int k = i - 1; k > 0; k--)
+                {
+                    if(argv[k][0] != '-')
+                    {
+                        sourceArray[count] = argv[k];
+                        count++;
+                    }
+                }
+                for (int k=i+2;k<argc;k++)
+                {
+                    if(argv[k][0] != '-')
+                    {
+                        sourceArray[count] = argv[k];
+                        count++;
+                    }
+                }
+                strcpy(destination,argv[i+1]);
+            }
+            else
+            {
+                if(i == 1)
+                {
+                    sourceArray[0] = argv[i+1];
+                    strcpy(destination,argv[i+2]);
+                }
+                else if(i == 2)
+                {
+                    sourceArray[0] = argv[i-1];
+                    strcpy(destination,argv[i+1]);
+                }
+                else if(i == 3)
+                {
+                    sourceArray[0] = argv[i-2];
+                    strcpy(destination,argv[i-1]);
+                }
+            }
+        }
+}
+
+int copy_folder(char *source_path, char *dest_path)
+{
+    char destFile[1024];
+
+    strcpy(destFile, dest_path);
+    strcat(destFile, "/");
+    strcat(destFile, source_path);
+
+    FILE *source = fopen(source_path, "r");
+
+    FILE *dest = fopen(destFile, "w");
+    char buffer[1024];
+    while (true) {
+        size_t bytes_read = fread(buffer, 1, sizeof(buffer), source);
+        if (bytes_read == 0)
+            break;
+        fwrite(buffer, 1, bytes_read, dest);
+    }
+    fclose(dest);
+    fclose(source);
+
+    return 0;
+}
+
+int copy_file(char *source_path, char *dest_path)
+{
+    char destFile[1024];
+
+    strcpy(destFile, dest_path);
+    FILE *source = fopen(source_path, "r");
+    ofstream newFile(destFile);
+
+    FILE *dest = fopen(destFile, "w");
+    char buffer[1024];
+    while (true) {
+        size_t bytes_read = fread(buffer, 1, sizeof(buffer), source);
+        if (bytes_read == 0)
+            break;
+        fwrite(buffer, 1, bytes_read, dest);
+    }
+    fclose(dest);
+    fclose(source);
+
+    return 0;
+}
+
 int main(int argc, char **argv)
 {
+    char *sourceArray[1024];
+    char destination[1024];
+    char *source_path = argv[1];
+    char *dest_path = argv[2];
     int hflag = 0;
     int iflag = 0;
     int rflag = 0;
@@ -64,17 +172,18 @@ int main(int argc, char **argv)
                 "  -v, --verbose                explain what is being done\n"
                 "      --help     display this help and exit\n"
                 "\n"
-                "Examples:\n"
-                "  dirname /usr/bin/          -> \"/usr\"\n"
-                "  dirname dir1/str dir2/str  -> \"dir1\" followed by \"dir2\"\n"
-                "  dirname stdio.h            -> \".\"\n"
-                "\n"
                 "GNU coreutils online help: <https://www.gnu.org/software/coreutils/>\n"
                 "Full documentation <https://www.gnu.org/software/coreutils/cp>\n"
                 "or available locally via: info '(coreutils) cp invocation'" << endl;
     else if(iflag != 0)
     {
-
+        argType(argc,argv,sourceArray,destination);
+        cout << "cp: overwrite '" << destination <<"'?";
+        getchar();
+        if (isDir(destination) == 0)
+            copy_folder(sourceArray[0],destination);
+        else
+            copy_file(sourceArray[0],destination);
     }
     else if(rflag != 0)
     {
@@ -82,19 +191,31 @@ int main(int argc, char **argv)
     }
     else if(tflag != 0)
     {
-
+        argType(argc,argv,sourceArray,destination);
+        for(int i=0;i<sizeof(sourceArray);i++)
+            copy_folder(sourceArray[i],destination);
     }
     else if(vflag != 0)
     {
-
+        argType(argc,argv,sourceArray,destination);
+        cout << "'" << sourceArray[0] << "' -> '" << destination << "'" << endl;
+        if (isDir(destination) == 0)
+            copy_folder(sourceArray[0],destination);
+        else
+            copy_file(sourceArray[0],destination);
     }
     else
     {
+        argType(argc,argv,sourceArray,destination);
         if (argc < 3) {
-            cerr << "cp: missing file operand\n"
+            cout << "cp: missing file operand\n"
                     "Try 'cp --help' for more information." << endl;
             return 1;
         }
+        if (isDir(dest_path) == 0)
+            copy_folder(source_path,dest_path);
+        else
+            copy_file(source_path,dest_path);
     }
     return 0;
 }
